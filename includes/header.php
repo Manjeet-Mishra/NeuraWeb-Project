8238b26e87dd05d9a5c0c42f0f5a69db96809707
Manjeet-Mishra <154807485+Manjeet-Mishra@users.noreply.github.com>
2025-03-07 04:04:04 +0000

    Updated files

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NeuraWeb</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header class="header-holder">
+        <div class="header-logo-holder">
+            <img src="assets/images/Neura_web_logo.png" alt="Logo of Company">
+        </div>
+        <nav>
+            <a href="pages/Home.php">NeuraSphere</a>
+            <a href="pages/about.php">Behind NeuraWeb</a>
+            <a href="pages/portfolio.php">Innovations Hub</a>
+            <a href="pages/services.php">Web Wizard</a>
+            <a href="pages/contact.php">NeuraConnect</a>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NeuraWeb-Where AI meets the web</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <script href="main.js"></script>
+</body>
+</html>
